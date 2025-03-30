0e2f0c1a048c2bc81a0d606c52079d9880d9fcd5
Choi Myeongguk <choimk0512@gmail.com>
2025-03-30 17:00:06 +0900

    first commit

---
+++ HW1/calculate.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int a, b, c, d;
+
+  cin >> a >> b;
+  c = a + b;
+  d = a - b;
+  cout << c << " " << d;
+  cout << "hello world!";
+
+  return 0;
+}
---
+++ HW1/helloworld.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  cout << "Hello World\n";
+  cout << "[2024320019]Choi Myeongguk";
+
+  return 0;
+}
